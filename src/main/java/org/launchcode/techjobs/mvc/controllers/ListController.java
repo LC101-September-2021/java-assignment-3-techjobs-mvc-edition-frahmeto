2071e1a748895f073f32ca7f539730564d88d5a0
Fidel Rahmeto <fidyod@gmail.com>
2022-03-08 19:46:11 -0600

    completed assignment 3

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -5,9 +5,7 @@ import org.launchcode.techjobs.mvc.models.Job;
 import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -4,9 +4,7 @@ import org.launchcode.techjobs.mvc.models.Job;
 import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -26,14 +24,14 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @RequestMapping("results")
+    @RequestMapping(value= "results", method = RequestMethod.POST)
     public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
         ArrayList<Job> jobs;
         if(searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")){
             jobs = JobData.findAll();
         }else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-            model.addAttribute("title", "Jobs with " + searchType + ":" + searchTerm);
+           // model.addAttribute("title", "Jobs with " + searchType + ":" + searchTerm);
         }
 
         model.addAttribute("jobs", jobs);
